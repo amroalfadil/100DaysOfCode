dfaf302dcbc59b32b965505122b3035261b5f3f7
Amro <58052565+amroalfadil@users.noreply.github.com>
2023-12-08 12:25:24 +0000

    created first iteration

---
+++ band_name_generator.py
@@ -0,0 +1,7 @@
+# ask the name of the city you grow up in 
+print('Welcom to the Band Name Generator')
+city = input('What\'s name of the city you grew up in ? ').capitalize()
+# ask for the name of the pet
+pet = input('What\'s your pet\'s name ? ').capitalize()
+# print band name combining both entries
+print(f'Your band name could be {city} {pet}')
